dc71fcc292949c64c54232cfddabcdcc38b78041
dheeraj1717 <dheerajjangid1717@gmail.com>
2026-02-16 13:16:52 +0530

    router and controller setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { PORT } from "./config/serverConfig";
+import apiRouter from "./routes";
 
 const app = express();
+
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Evaluator service is running on port ${PORT}`);
