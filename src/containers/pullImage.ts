bfbe040693ef82eeea35cd35491a8ba14b2ce56c
dheeraj1717 <dheerajjangid1717@gmail.com>
2026-03-08 23:49:30 +0530

    fix: pull image function

--- src/containers/pullImage.ts
+++ src/containers/pullImage.ts
@@ -1,20 +1,24 @@
 import Docker from 'dockerode';
 
-export default async function pullImage(imageName: string) {
+export default async function pullImage(imageName: string): Promise<void> {
     try {
         const docker = new Docker();
-        const image = await docker.pull(imageName, (err: Error | null, stream: NodeJS.ReadableStream) => {
-            if (err) {
-                console.log(err);
-            }
-            docker.modem.followProgress(stream, (err: Error | null, res: any) => {
+        return new Promise((resolve, reject) => {
+            docker.pull(imageName, (err: Error | null, stream: NodeJS.ReadableStream) => {
                 if (err) {
                     console.log(err);
+                    return reject(err);
                 }
-                console.log(res);
+                docker.modem.followProgress(stream, (err: Error | null, res: any) => {
+                    if (err) {
+                        console.log(err);
+                        return reject(err);
+                    }
+                    console.log(`Successfully pulled image: ${imageName}`);
+                    resolve();
+                });
             });
         });
-        return image;
     } catch (error) {
         console.log(error);
     }
