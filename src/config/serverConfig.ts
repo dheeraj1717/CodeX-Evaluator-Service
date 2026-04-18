dae6a665543a5fa55efae882a4295010075d6ce1
dheeraj1717 <dheerajjangid1717@gmail.com>
2026-04-18 22:53:27 +0530

    updated config

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -4,3 +4,4 @@ export const PORT = process.env.PORT || 4001;
 export const REDIS_PORT = parseInt(process.env.REDIS_PORT || "6379", 10);
 export const REDIS_HOST = process.env.REDIS_HOST || "[IP_ADDRESS]";
 export const LOG_DB_URI = process.env.LOG_DB_URI || "";
+export const NODE_ENV = process.env.NODE_ENV || "development";
